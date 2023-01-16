d085d10d07e8f74878fe3ec927cc09fdde7eac32
Gever Tulley <gever@sfbrightworks.org>
2023-01-16 10:34:50 -0800

    minor comment cleanup

--- boxbot.ino
+++ boxbot.ino
@@ -1,14 +1,5 @@
 /*
-  WiFiAccessPoint.ino creates a WiFi access point and provides a web server on it.
-
-  Steps:
-  1. Connect to the access point "yourAp"
-  2. Point your web browser to http://192.168.4.1/H to turn the LED on or http://192.168.4.1/L to turn it off
-     OR
-     Run raw TCP "GET /H" and "GET /L" on PuTTY terminal with 192.168.4.1 as IP address and 80 as port
-
-  Created for arduino-esp32 on 04 July, 2018
-  by Elochukwu Ifediora (fedy0)
+  based on WiFiAccessPoint.ino by Elochukwu Ifediora (fedy0)
 */
 
 #include <WiFi.h>
@@ -30,7 +21,7 @@ float linear_motion_fudge = 1.0;
 bool wheels_forward = true;
 
 // Set these to your desired credentials.
-const char *ssid = "CSSC-Albion";
+const char *ssid = "silly-pickle";
 const char *password = (char *)NULL;
 
 WebServer server(80);
